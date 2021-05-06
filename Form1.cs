abbe3bfaad594a41471cfe1b6f50f7d5f63c85b5
luyuh <774964107@qq.com>
2021-05-06 17:30:16 +0800

    first commit

---
+++ Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Drawing.Imaging;
+
+namespace WinFormScreenShoot
+{
+    public partial class Form1 : Form
+    {
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.ContextMenu contextMenu1;
+        private System.Windows.Forms.MenuItem menuItem1, menuItem2;
+
+        public Form1()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.contextMenu1 = new System.Windows.Forms.ContextMenu();
+            this.menuItem1 = new System.Windows.Forms.MenuItem();
+            this.menuItem2 = new System.Windows.Forms.MenuItem();
+
+            // Initialize contextMenu1
+            this.contextMenu1.MenuItems.AddRange(
+                        new System.Windows.Forms.MenuItem[] { this.menuItem1, this.menuItem2 }) ;
+
+            this.FormBorderStyle = FormBorderStyle.None;
+            this.WindowState = FormWindowState.Minimized;
+            this.Location = new System.Drawing.Point(-1000, -1000);
+            this.Size = new System.Drawing.Size(0, 0);
+            this.Visible = false;
+
+            // Initialize menuItem1
+            this.menuItem1.Index = 0;
+            this.menuItem1.Text = "E&xit";
+            this.menuItem1.Click += new System.EventHandler(this.menuItem1_Click);
+            this.menuItem2.Index = 1;
+            this.menuItem2.Text = "Say Something";
+            this.menuItem2.Click += new System.EventHandler(this.menuItem2_Click);
+
+            // Set up how the form should be displayed.
+            this.ClientSize = new System.Drawing.Size(292, 266);
+            this.Text = "Notify Icon Example";
+
+            // Create the NotifyIcon.
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+
+            // The Icon property sets the icon that will appear
+            // in the systray for this application.
+            notifyIcon1.Icon = new Icon("appicon.ico");
+
+            // The ContextMenu property sets the menu that will
+            // appear when the systray icon is right clicked.
+            notifyIcon1.ContextMenu = this.contextMenu1;
+
+            // The Text property sets the text that will be displayed,
+            // in a tooltip, when the mouse hovers over the systray icon.
+            notifyIcon1.Text = "Form1 (NotifyIcon example)";
+            notifyIcon1.Visible = true;
+
+            // Handle the DoubleClick event to activate the form.
+            notifyIcon1.DoubleClick += new System.EventHandler(this.notifyIcon1_DoubleClick);
+        }
+
+
+        private void notifyIcon1_DoubleClick(object Sender, EventArgs e)
+        {
+            // Show the form when the user double clicks on the notify icon.
+
+            // Set the WindowState to normal if the form is minimized.
+            if (this.WindowState == FormWindowState.Minimized)
+                this.WindowState = FormWindowState.Normal;
+
+            // Activate the form.
+            this.Activate();
+        }
+
+        private void menuItem1_Click(object Sender, EventArgs e)
+        {
+            // Close the form, which closes the application.
+            this.Close();
+        }
+        private void menuItem2_Click(object Sender, EventArgs e)
+        {
+            // Close the form, which closes the application.
+
+            Debug.WriteLine("Helloooooo");
+            // CaptureScreen(0, 0, 100, 100);
+            Form2 frm = new Form2();
+            frm.Show();
+        }
+
+        
+    }
+}
---
+++ Form2.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Media;
+
+namespace WinFormScreenShoot
+{
+    public partial class Form2 : Form
+    {
+        private double x;
+        private double y;
+        private double width;
+        private double height;
+
+        private bool isMouseDown = false;
+
+      
+
+        public Form2()
+        {
+            InitializeComponent();
+
+            this.FormBorderStyle = FormBorderStyle.None;
+            this.WindowState = FormWindowState.Maximized;
+            this.Location = new System.Drawing.Point(0, 0);
+            MouseDown += this.Form2_MouseClick;
+            
+        }
+
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public void Form2_MouseClick(object sender, MouseEventArgs e)
+        {
+            isMouseDown = true;
+            x = e.X;
+            y = e.Y;
+        }
+
+        private void CaptureWindow_MouseMove(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            if (isMouseDown)
+            {
+                // 1. 通过一个矩形来表示目前截图区域
+                System.Drawing.Imaging.Rectangle rect = new System.Drawing.Rectangle();
+                double dx = e.GetPosition(null).X;
+                double dy = e.GetPosition(null).Y;
+                double rectWidth = Math.Abs(dx - x);
+                double rectHeight = Math.Abs(dy - y);
+                SolidColorBrush brush = new SolidColorBrush(Colors.White);
+                rect.Width = (int)rectWidth;
+                rect.Height = (int)rectHeight;
+                rect.Fill = brush;
+                rect.Stroke = brush;
+                rect.StrokeThickness = 1;
+                if (dx < x)
+                {
+                    Canvas.SetLeft(rect, dx);
+                    Canvas.SetTop(rect, dy);
+                }
+                else
+                {
+                    Canvas.SetLeft(rect, x);
+                    Canvas.SetTop(rect, y);
+                }
+
+                CaptureCanvas.Children.Clear();
+                CaptureCanvas.Children.Add(rect);
+
+                if (e.LeftButton == MouseButtonState.Released)
+                {
+                    CaptureCanvas.Children.Clear();
+                    // 2. 获得当前截图区域
+                    width = Math.Abs(e.GetPosition(null).X - x);
+                    height = Math.Abs(e.GetPosition(null).Y - y);
+
+                    if (e.GetPosition(null).X > x)
+                    {
+                        CaptureScreen(x, y, width, height);
+                    }
+                    else
+                    {
+                        CaptureScreen(e.GetPosition(null).X, e.GetPosition(null).Y, width, height);
+                    }
+
+
+                    isMouseDown = false;
+                    x = 0.0;
+                    y = 0.0;
+                    this.Close();
+                }
+            }
+        }
+
+
+        private void CaptureScreen(double x, double y, double width, double height)
+        {
+            int ix = Convert.ToInt32(x);
+            int iy = Convert.ToInt32(y);
+            int iw = Convert.ToInt32(width);
+            int ih = Convert.ToInt32(height);
+
+            System.Drawing.Bitmap bitmap = new Bitmap(iw, ih);
+            using (System.Drawing.Graphics graphics = Graphics.FromImage(bitmap))
+            {
+                graphics.CopyFromScreen(ix, iy, 0, 0, new System.Drawing.Size(iw, ih));
+
+                SaveFileDialog dialog = new SaveFileDialog();
+                dialog.Filter = "Png Files|*.png";
+                if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                {
+                    bitmap.Save(dialog.FileName, ImageFormat.Png);
+                }
+            }
+        }
+    }
+}
